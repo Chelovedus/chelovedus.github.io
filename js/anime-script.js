f5a13d55fd3a37e1bbcfd1b0e2c1dd09b5db1576
Vladislav <dkflbr13@yandex.ru>
2026-02-01 05:02:49 +0300

    Js code refactoring. Add anime titles

--- js/anime-script.js
+++ js/anime-script.js
@@ -11,39 +11,25 @@ async function loadAnimeData() {
 
 function creatorElement(fileData)
 {
-    //console.log(fileData);
-    //console.log(fileLength);
-
-    const element = template.content.cloneNode(true);
-    
-    let block = element.querySelector('.title-block');
-
     let i = 0;
     while (i < fileData.length) {
         const element = template.content.cloneNode(true);
         const block = element.querySelector('.title-block');
-        
-        if (block) {
-            // Fill with current anime's data
-            const nameElement = element.querySelector('.title-name');
-            const descElement = element.querySelector('.title-description');
-            const genresElement = element.querySelector('.title-genres');
-            
-            if (nameElement) nameElement.textContent = fileData[i].name;
-            if (descElement) descElement.textContent = fileData[i].description;
-            if (genresElement) genresElement.textContent = fileData[i].genres;
+        const nameElement = element.querySelector('.title-name');
+        const descriptionElement = element.querySelector('.title-description');
+        const genresElement = element.querySelector('.title-genres');
 
-            block.style.backgroundImage = `url(/img/anime-titles/Charlotte.jpg)`;
-            
-            container.appendChild(element);
-        }
-        
+        createElement(i, fileData, element, block, nameElement, descriptionElement, genresElement);
         i++;
     }
 }
 
-function createElement()
+function createElement(i, fileData, element, block, nameElement, descriptionElement, genresElement)
 {
-    
+    if (nameElement) nameElement.textContent = fileData[i].title;
+    if (descriptionElement) descriptionElement.textContent = fileData[i].description;
+    if (genresElement) genresElement.textContent = fileData[i].genres;
+    block.style.backgroundImage = `url(${fileData[i].image})`;
+    container.appendChild(element);
 }
 
